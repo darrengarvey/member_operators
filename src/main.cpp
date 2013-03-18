e97396fa40ecd5a7a05e3b831678107f353b7cdd
Darren Garvey <darren.garvey@youview.com>
2013-03-18 00:10:00 +0000

    Add lots of variations to generate less than functors based on a class member function.
    Compiled sizes using clang++ and g++ in bytes are below. Both compiled with -O3.
    
    36618 test_with_gcc_type1
    23086 test_with_gcc_type2
    23126 test_with_gcc_type3
    23176 test_with_gcc_type4
    23276 test_with_gcc_type5
    37190 test_with_gcc_type6
    23272 test_with_gcc_type7
    17984 test_with_no_operator_wrapper
    46744 test_with_operator_wrapper
    81298 test_with_operator_wrapper_o0
    65187 test_with_operator_wrapper_o1
    46896 test_with_operator_wrapper_o2
    46744 test_with_type1
    23454 test_with_type2
    23516 test_with_type3
    23561 test_with_type4
    23772 test_with_type5
    47308 test_with_type6
    23855 test_with_type7
    
    Compiler versions used:
    
    [darren.garvey@localhost build]$ clang++ -v
    clang version 3.3 (trunk 176628)
    Target: i386-pc-linux-gnu
    Thread model: posix
    [darren.garvey@localhost build]$ g++ -v
    Using built-in specs.
    COLLECT_GCC=/usr/bin/g++
    COLLECT_LTO_WRAPPER=/usr/libexec/gcc/i686-redhat-linux/4.7.2/lto-wrapper
    Target: i686-redhat-linux
    Configured with: ../configure --prefix=/usr --mandir=/usr/share/man --infodir=/usr/share/info --with-bugurl=http://bugzilla.redhat.com/bugzilla --enable-bootstrap --enable-shared --enable-threads=posix --enable-checking=release --disable-build-with-cxx --disable-build-poststage1-with-cxx --with-system-zlib --enable-__cxa_atexit --disable-libunwind-exceptions --enable-gnu-unique-object --enable-linker-build-id --with-linker-hash-style=gnu --enable-languages=c,c++,objc,obj-c++,java,fortran,ada,go,lto --enable-plugin --enable-initfini-array --enable-java-awt=gtk --disable-dssi --with-java-home=/usr/lib/jvm/java-1.5.0-gcj-1.5.0.0/jre --enable-libgcj-multifile --enable-java-maintainer-mode --with-ecj-jar=/usr/share/java/eclipse-ecj.jar --disable-libjava-multilib --with-ppl --with-cloog --with-tune=generic --with-arch=i686 --build=i686-redhat-linux
    Thread model: posix
    gcc version 4.7.2 20121109 (Red Hat 4.7.2-8) (GCC)

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,190 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <typeinfo>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+#include <boost/assign/list_of.hpp>
 
 using namespace std;
+using namespace boost::assign;
+
+struct Type
+{
+    Type(string _s, int _num) : s(_s), num(_num) {}
+
+    string s;
+    int num;
+};
+
+#if defined(TYPE1)
+template<typename Struct, typename T>
+struct OperatorWrapper
+{
+    typedef T (Struct::*member_ptr);
+
+    OperatorWrapper(T Struct::*member_ptr)
+        : member(member_ptr)
+    {
+    }
+
+    bool operator()(const Struct& s1, const Struct& s2) const
+    {
+        return s1.*member < s2.*member;
+    }
+
+    member_ptr member;
+};
+
+template< typename T, typename Struct >
+boost::function< bool(const Struct&, const Struct&) >
+less_than(T Struct::*member_ptr)
+{
+    return OperatorWrapper<Struct, T>(member_ptr);
+}
+#elif defined(TYPE2)
+bool less_than(const Type& t1, const Type& t2)
+{
+    return t1.s < t2.s;
+}
+#elif defined(TYPE3)
+struct LessThan
+{
+    bool operator()(const Type& t1, const Type& t2)
+    {
+        return t1.s < t2.s;
+    }
+};
+#elif defined(TYPE4)
+template< typename Struct >
+struct LessThan
+{
+    bool operator()(const Struct& t1, const Struct& t2)
+    {
+        return t1.s < t2.s;
+    }
+};
+#elif defined(TYPE5)
+template<typename Struct, typename T>
+struct OperatorWrapper
+{
+    typedef T (Struct::* const member_ptr);
+
+    OperatorWrapper(T Struct::* const member_ptr)
+        : member(member_ptr)
+    {
+    }
+
+    bool operator()(const Struct& s1, const Struct& s2) const
+    {
+        return s1.*member < s2.*member;
+    }
+
+    const member_ptr member;
+};
+
+template< typename T, typename Struct >
+OperatorWrapper<Struct, T>
+less_than(T Struct::*member_ptr)
+{
+    return OperatorWrapper<Struct, T>(member_ptr);
+}
+#elif defined(TYPE6)
+boost::function< bool(const Type&, const Type&) >
+less_than()
+{
+    return boost::bind(&Type::s, _1) < boost::bind(&Type::s, _2);
+}
+#elif defined(TYPE7)
+template<typename T, typename Struct, T Struct::*member_ptr>
+struct OperatorWrapper
+{
+    static T Struct::* const member;
+
+    bool operator()(const Struct& s1, const Struct& s2) const
+    {
+        return s1.*member < s2.*member;
+    }
+};
+
+template<typename T, typename Struct, T Struct::*member_ptr>
+T Struct::* const OperatorWrapper<T, Struct, member_ptr>::member = member_ptr;
+
+template< typename T, typename Struct, T Struct::*member_ptr >
+OperatorWrapper<T, Struct, member_ptr>
+less_than()
+{
+    return OperatorWrapper<T, Struct, member_ptr>();
+}
+#elif defined(TYPE8)
+template<typename T, typename Struct, T Struct::*member_ptr>
+struct OperatorWrapper
+{
+    static T Struct::* const member;
+
+    bool operator()(const Struct& s1, const Struct& s2) const
+    {
+        return s1.*member < s2.*member;
+    }
+};
+
+template<typename T, typename Struct, T Struct::*member_ptr>
+T Struct::* const OperatorWrapper<T, Struct, member_ptr>::member = member_ptr;
+
+template< typename T, typename Struct >
+OperatorWrapper<T, Struct, member_ptr>
+less_than(T Struct::*member_ptr)
+{
+    return OperatorWrapper<T, Struct, member_ptr>();
+}
+#endif
+
+template< typename T, typename V > 
+void print( vector<T>& stuff, V (T::* member_ptr) )
+{
+    for(size_t i=0; i < stuff.size(); ++i)
+    {   
+        if (i) cout<< ", ";
+        cout<< stuff[i].*member_ptr;
+    }   
+    cout<< endl;
+}
 
 int main(int, char**)
 {
-    cout<< "Hello" << endl;
+    vector< Type > types = list_of
+        ( Type("one", 1) )
+        ( Type("two", 2 ) )
+        ( Type("three", 3 ) )
+        ( Type("f    our", 4 ) );
+
+#if defined(TYPE1)
+    sort( types.begin(), types.end(), less_than(&Type::s) );
+    //less_than(&Type::s);
+#elif defined(TYPE2)
+    sort( types.begin(), types.end(), &less_than );
+    //&less_than;
+#elif defined(TYPE3)
+    sort( types.begin(), types.end(), LessThan() );
+    //LessThan();
+#elif defined(TYPE4)
+    sort( types.begin(), types.end(), LessThan<Type>() );
+    //LessThan<Type>();
+#elif defined(TYPE5)
+    sort( types.begin(), types.end(), less_than(&Type::s) );
+    //less_than(&Type::s);
+#elif defined(TYPE6)
+    sort( types.begin(), types.end(), less_than() );
+    //less_than();
+#elif defined(TYPE7)
+    sort( types.begin(), types.end(), less_than<string, Type, &Type::s>() );
+    //less_than<string, Type, &Type::s>();
+#elif defined(TYPE8)
+    sort( types.begin(), types.end(), less_than(&Type::s) );
+    //less_than(&Type::s);
+#endif
+
+    print( types, &Type::s );
+
     return 0;
 }
